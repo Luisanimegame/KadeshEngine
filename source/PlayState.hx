83469876e13a4ace5db5b71ede18263323d3fc17
GaboWuz ★ <96131201+Luisanimegame@users.noreply.github.com>
2025-10-31 20:52:02 -0300

    E pode isso 2?

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2680,30 +2680,28 @@ class PlayState extends MusicBeatState
 				reloadHealthBarColors();
 
 			case 'Change Scroll Speed':
-		    if (songSpeedType == "constant") return;
-		
-		    var val1:Float = Std.parseFloat(value1);
-		    var val2:Float = Std.parseFloat(value2);
-		    if (Math.isNaN(val1)) val1 = 1;
-		    if (Math.isNaN(val2)) val2 = 0;
-		
-		    var targetSpeed:Float = songSpeed * val1;
-		
-		    if (val2 <= 0) {
-		        songSpeed = targetSpeed;
-		        if (songSpeedTween != null) {
-		            songSpeedTween.cancel();
-		            songSpeedTween = null;
-		        }
-		    } else {
-		        if (songSpeedTween != null) songSpeedTween.cancel();
-		        songSpeedTween = FlxTween.tween(this, { songSpeed: targetSpeed }, val2, {
-		            ease: FlxEase.linear,
-		            onComplete: function(twn:FlxTween) {
-		                songSpeedTween = null;
-		            }
-		        });
-		    }
+				if (songSpeedType == "constant")
+					return;
+				var val1:Float = Std.parseFloat(value1);
+				var val2:Float = Std.parseFloat(value2);
+				if(Math.isNaN(val1)) val1 = 1;
+				if(Math.isNaN(val2)) val2 = 0;
+
+				var newValue:Float = SONG.speed * ClientPrefs.getGameplaySetting('scrollspeed', 1) * val1;
+
+				if(val2 <= 0)
+				{
+					songSpeed = newValue;
+				}
+				else
+				{
+					songSpeedTween = FlxTween.tween(this, {songSpeed: newValue}, val2 / playbackRate, {ease: FlxEase.linear, onComplete:
+						function (twn:FlxTween)
+						{
+							songSpeedTween = null;
+						}
+					});
+				}
 		}
 	}
 
